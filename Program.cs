032baa33f78701b07cb22315f6efc836c464bc9e
hernan-Palacios <hernanpalacios147736@correo.itm.edu.co>
2024-04-05 21:39:57 -0500

    Se testea 2 jugadores y es exitoso

--- Program.cs
+++ Program.cs
@@ -7,28 +7,28 @@ internal class Program
     {
     /*Juego:
         Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en 
-adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango y lo guarda en
-memoria sin ser revelado. 
-Por ejemplo: El sistema escoge aleatoriamente un número entre 0 y 100, ese número NO SE PUEDE
-REVELAR ya que es el número que se adivinará, por ende, dicho número se almacenará en memoria. 
-Los participantes comenzarán en orden a jugar y se turnarán para ingresar por pantalla un número
-ℕ hasta acertar el número oculto y salir ganador.
-El juego debe tener la funcionalidad de escoger el número de participantes que jugarán: mínimo 2 y
-máximo 4 integrantes.Dependiendo de la cantidad de jugadores, el número ℕ aleatorio se generará
-en los siguientes rangos: - - -
-Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50
-Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100
-Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200
-Cada jugador comenzará su turno intentando adivinar ese número ℕ aleatorio.El programa deberá
-mostrarle al jugador la siguiente información: - - -
-Si el número ingresado es mayor al número aleatorio, entonces mostrar por pantalla la
-palabra “MENOR” y darle la oportunidad al siguiente jugador.
-Si el número ingresado es menor al número aleatorio, entonces mostrar por pantalla la
-palabra “MAYOR” y darle la oportunidad al siguiente jugador.
-Si el número ingresado coincide con el número aleatorio, entonces mostrar por pantalla la
-palabra “¡HAS GANADO!”. Aquí ya finaliza el juego.
-El programa deberá estar en capacidad de pedir a los jugadores si desean un nuevo “tirito” para
-volver a jugar y borrar consola, de lo contrario, finalizar el programa.*/
+    adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango y lo guarda en
+    memoria sin ser revelado. 
+    Por ejemplo: El sistema escoge aleatoriamente un número entre 0 y 100, ese número NO SE PUEDE
+    REVELAR ya que es el número que se adivinará, por ende, dicho número se almacenará en memoria. 
+    Los participantes comenzarán en orden a jugar y se turnarán para ingresar por pantalla un número
+    ℕ hasta acertar el número oculto y salir ganador.
+    El juego debe tener la funcionalidad de escoger el número de participantes que jugarán: mínimo 2 y
+    máximo 4 integrantes.Dependiendo de la cantidad de jugadores, el número ℕ aleatorio se generará
+    en los siguientes rangos: - - -
+    Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50
+    Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100
+    Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200
+    Cada jugador comenzará su turno intentando adivinar ese número ℕ aleatorio.El programa deberá
+    mostrarle al jugador la siguiente información: - - -
+    Si el número ingresado es mayor al número aleatorio, entonces mostrar por pantalla la
+    palabra “MENOR” y darle la oportunidad al siguiente jugador.
+    Si el número ingresado es menor al número aleatorio, entonces mostrar por pantalla la
+    palabra “MAYOR” y darle la oportunidad al siguiente jugador.
+    Si el número ingresado coincide con el número aleatorio, entonces mostrar por pantalla la
+    palabra “¡HAS GANADO!”. Aquí ya finaliza el juego.
+    El programa deberá estar en capacidad de pedir a los jugadores si desean un nuevo “tirito” para
+    volver a jugar y borrar consola, de lo contrario, finalizar el programa.*/
 
         //Declaracion de variables
         Random numero = new Random ();
@@ -39,6 +39,7 @@ volver a jugar y borrar consola, de lo contrario, finalizar el programa.*/
         int numero1 = 51;
         int numero2 = 101;
         int numero3 = 201;
+        int intentos = 0;
         int jugadores;
         string juego1;
         string juego2;
@@ -49,23 +50,21 @@ volver a jugar y borrar consola, de lo contrario, finalizar el programa.*/
         jugadores = int.Parse(Console.ReadLine());
         Console.WriteLine($"haz seleccionado {jugadores} Jugadores");
 
-        if ( jugadores == 2 ) {
-
+        if ( jugadores == 2 )
+        {
+            Console.WriteLine("Empecemos a jugar")
             while (aleatorio1 != numero1)
             {
+                intentos++;
                 numero1 = int.Parse(Console.ReadLine());
                 if (numero1 > aleatorio1) Console.WriteLine("Es MENOR");
                 if (numero1 < aleatorio1) Console.WriteLine("Es MAYOR");
-                if (numero1 < aleatorio1) Console.WriteLine("¡HAS GANADO!");
+                if (numero1 == aleatorio1) Console.WriteLine("¡HAS GANADO!");
+
+                    Console.WriteLine(" Quieres jugar de nuevo");
 
             }
            
-        }
-
-                  
-
-            
-
         }
 
 
@@ -73,4 +72,9 @@ volver a jugar y borrar consola, de lo contrario, finalizar el programa.*/
 
 
     }
+
+
+
+
+
 }
